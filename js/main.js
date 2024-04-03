726c78ef0a640d510dee6fb4ee3b37a652298b26
S3N1OR <126277562+S3N1OR@users.noreply.github.com>
2024-04-04 01:06:26 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,225 +14,218 @@ function getRandomComment() {
     fetch(
       `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&maxResults=100&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk`
     )
-      
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.items || data.items.length === 0) {
+        alert("Нет доступных комментариев для розыгрыша");
+        return;
+      }
 
+      data.items.forEach((commentThread) => {
+        const comment = commentThread.snippet.topLevelComment.snippet.textDisplay;
+      });
+
+      const filteredCommentThreads = data.items.filter(
+        (commentThread) =>
+          !participatingUsers.includes(
+            commentThread.snippet.topLevelComment.snippet.authorDisplayName
+          )
+      );
+
+      if (filteredCommentThreads.length === 0) {
+        alert("Нет доступных комментариев для розыгрыша");
+        return;
+      }
+
+      const randomCommentThread =
+        filteredCommentThreads[
+          Math.floor(Math.random() * filteredCommentThreads.length)
+        ];
+      const comment =
+        randomCommentThread.snippet.topLevelComment.snippet.textDisplay;
+
+      const totalComments = data.items.length;
+
+      const userName =
+        randomCommentThread.snippet.topLevelComment.snippet.authorDisplayName;
+      const channelId =
+        randomCommentThread.snippet.topLevelComment.snippet.authorChannelId
+          .value;
+
+      participatingUsers.push(userName);
+
+      fetch(
+        `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk`
+      )
       .then((response) => response.json())
-      .then((data) => {
-        if (!data.items || data.items.length === 0) {
-          alert("Нет доступных комментариев для розыгрыша");
-          return;
-        }
+      .then((channelData) => {
+        const avatarUrl =
+          channelData.items[0].snippet.thumbnails.default.url;
 
-        console.log("Все комментарии из под видео:");
-        data.items.forEach((commentThread) => {
-          const comment = commentThread.snippet.topLevelComment.snippet.textDisplay;
-          console.log(comment);
+        const commentElement = document.createElement("div");
+        commentElement.classList.add("comment");
+        commentElement.textContent = "Комментарий: ";
+        const commentSpan = document.createElement("span");
+        commentSpan.innerHTML = comment.substring(0, 100);
+        commentElement.appendChild(commentSpan);
+
+        const userNameElement = document.createElement("div");
+        userNameElement.classList.add("name");
+        userNameElement.innerHTML = "NickName: ";
+        const userNameSpan = document.createElement("span");
+        userNameSpan.innerHTML = userName;
+        userNameElement.appendChild(userNameSpan);
+        const copyName = document.createElement("div");
+        copyName.classList.add("copy__name");
+        userNameElement.appendChild(copyName);
+
+        const totalCommentsElement = document.createElement("div");
+        totalCommentsElement.classList.add("total__comments");
+        totalCommentsElement.textContent = "Всего комментариев под видео: ";
+        const totalCommentsSpan = document.createElement("span");
+        totalCommentsSpan.innerHTML = totalComments;
+        totalCommentsElement.appendChild(totalCommentsSpan);
+
+        const winnerCommentsElement = document.createElement("div");
+        winnerCommentsElement.classList.add("winner__comments");
+        winnerCommentsElement.textContent =
+          "Количество комментариев от победителя: ";
+        const winnerCommentsSpan = document.createElement("span");
+        winnerCommentsElement.appendChild(winnerCommentsSpan);
+
+        const avatarElement = document.createElement("img");
+        avatarElement.src =
+          "https://yt3.googleusercontent" + avatarUrl.replace(/^.{17}/, "");
+        avatarElement.alt = "Аватар комментатора";
+        avatarElement.classList.add("avatar");
+        const avatarIcon = document.createElement("img");
+        avatarIcon.src = "./img/win-icon.png";
+        avatarIcon.classList.add("win__icon");
+
+        const winContent = document.getElementById("win");
+        const winBlock = document.createElement("win");
+        const winTitle = document.createElement("h4");
+        winBlock.classList.add("win__block");
+        winTitle.classList.add("win__title");
+        winTitle.innerHTML = "Победитель";
+        winContent.innerHTML = "";
+        winContent.appendChild(winTitle);
+        winContent.appendChild(winBlock);
+        winBlock.appendChild(avatarElement);
+        winBlock.appendChild(avatarIcon);
+
+        copyName.addEventListener("click", function () {
+          var copyNameText = userName;
+
+          navigator.clipboard.writeText(copyNameText);
         });
 
-        const filteredCommentThreads = data.items.filter(
-          (commentThread) =>
-            !participatingUsers.includes(
-              commentThread.snippet.topLevelComment.snippet.authorDisplayName
-            )
-        );
+        const redDots = document.createElement("div");
+        redDots.classList.add("dots");
+        redDots.classList.add("red");
+        const orangeDots = document.createElement("div");
+        orangeDots.classList.add("dots");
+        orangeDots.classList.add("orange");
+        const greenDots = document.createElement("div");
+        greenDots.classList.add("dots");
+        greenDots.classList.add("green");
 
-        if (filteredCommentThreads.length === 0) {
-          alert("Нет доступных комментариев для розыгрыша");
-          return;
-        }
+        const randomCommentTop = document.createElement("div");
+        randomCommentTop.classList.add("random-comment-top");
 
-        const randomCommentThread =
-          filteredCommentThreads[
-            Math.floor(Math.random() * filteredCommentThreads.length)
-          ];
-        const comment =
-          randomCommentThread.snippet.topLevelComment.snippet.textDisplay;
+        const channelLink = document.createElement("a");
+        channelLink.classList.add("channel__link");
+        channelLink.href = "https://www.youtube.com/channel/" + channelId;
+        channelLink.target = "_blank";
+        userNameElement.appendChild(channelLink);
 
-        const totalComments = data.items.length;
-        console.log("Общее количество комментариев под видео:", data.items.length);
-
-        const userName =
-          randomCommentThread.snippet.topLevelComment.snippet.authorDisplayName;
-        const channelId =
-          randomCommentThread.snippet.topLevelComment.snippet.authorChannelId
-            .value;
-
-        participatingUsers.push(userName);
+        document.getElementById("random-comment").innerHTML = "";
+        document.getElementById("random-comment").appendChild(redDots);
+        document.getElementById("random-comment").appendChild(orangeDots);
+        document.getElementById("random-comment").appendChild(greenDots);
+        document
+          .getElementById("random-comment")
+          .appendChild(userNameElement);
+        document
+          .getElementById("random-comment")
+          .appendChild(commentElement);
+        document
+          .getElementById("random-comment")
+          .appendChild(totalCommentsElement);
+        document
+          .getElementById("random-comment")
+          .appendChild(winnerCommentsElement);
 
         fetch(
-          `https://www.googleapis.com/youtube/v3/channels?part=snippet&id=${channelId}&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk`
+          `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&maxResults=100&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk&authorChannelId=${channelId}`
         )
-          .then((response) => response.json())
-          .then((channelData) => {
-            const avatarUrl =
-              channelData.items[0].snippet.thumbnails.default.url;
+        .then((response) => response.json())
+        .then((data) => {
+          const commentThreads = data.items;
+          const winnerCommentsCount = commentThreads.filter(
+            (commentThread) =>
+              commentThread.snippet.topLevelComment.snippet
+                .authorChannelId.value === channelId
+          ).length;
 
-            const commentElement = document.createElement("div");
-            commentElement.classList.add("comment");
-            commentElement.textContent = "Комментарий: ";
-            const commentSpan = document.createElement("span");
-            commentSpan.innerHTML = comment.substring(0, 100);
-            commentElement.appendChild(commentSpan);
-
-            const userNameElement = document.createElement("div");
-            userNameElement.classList.add("name");
-            userNameElement.innerHTML = "NickName: ";
-            const userNameSpan = document.createElement("span");
-            userNameSpan.innerHTML = userName;
-            userNameElement.appendChild(userNameSpan);
-            const copyName = document.createElement("div");
-            copyName.classList.add("copy__name");
-            userNameElement.appendChild(copyName);
-
-            const totalCommentsElement = document.createElement("div");
-            totalCommentsElement.classList.add("total__comments");
-            totalCommentsElement.textContent = "Всего комментариев под видео: ";
-            const totalCommentsSpan = document.createElement("span");
-            totalCommentsSpan.innerHTML = totalComments;
-            totalCommentsElement.appendChild(totalCommentsSpan);
-
-            const winnerCommentsElement = document.createElement("div");
-            winnerCommentsElement.classList.add("winner__comments");
-            winnerCommentsElement.textContent =
-              "Количество комментариев от победителя: ";
-            const winnerCommentsSpan = document.createElement("span");
-            winnerCommentsElement.appendChild(winnerCommentsSpan);
-
-            const avatarElement = document.createElement("img");
-            avatarElement.src =
-              "https://yt3.googleusercontent" + avatarUrl.replace(/^.{17}/, "");
-            avatarElement.alt = "Аватар комментатора";
-            avatarElement.classList.add("avatar");
-            const avatarIcon = document.createElement("img");
-            avatarIcon.src = "./img/win-icon.png";
-            avatarIcon.classList.add("win__icon");
-
-            const winContent = document.getElementById("win");
-            const winBlock = document.createElement("win");
-            const winTitle = document.createElement("h4");
-            winBlock.classList.add("win__block");
-            winTitle.classList.add("win__title");
-            winTitle.innerHTML = "Победитель";
-            winContent.innerHTML = "";
-            winContent.appendChild(winTitle);
-            winContent.appendChild(winBlock);
-            winBlock.appendChild(avatarElement);
-            winBlock.appendChild(avatarIcon);
-
-            copyName.addEventListener("click", function () {
-              var copyNameText = userName;
-
-              navigator.clipboard.writeText(copyNameText);
-            });
-
-            const redDots = document.createElement("div");
-            redDots.classList.add("dots");
-            redDots.classList.add("red");
-            const orangeDots = document.createElement("div");
-            orangeDots.classList.add("dots");
-            orangeDots.classList.add("orange");
-            const greenDots = document.createElement("div");
-            greenDots.classList.add("dots");
-            greenDots.classList.add("green");
-
-            const randomCommentTop = document.createElement("div");
-            randomCommentTop.classList.add("random-comment-top");
-
-            const channelLink = document.createElement("a");
-            channelLink.classList.add("channel__link");
-            channelLink.href = "https://www.youtube.com/channel/" + channelId;
-            channelLink.target = "_blank";
-            userNameElement.appendChild(channelLink);
-
-            document.getElementById("random-comment").innerHTML = "";
-            document.getElementById("random-comment").appendChild(redDots);
-            document.getElementById("random-comment").appendChild(orangeDots);
-            document.getElementById("random-comment").appendChild(greenDots);
-            document
-              .getElementById("random-comment")
-              .appendChild(userNameElement);
-            document
-              .getElementById("random-comment")
-              .appendChild(commentElement);
-            document
-              .getElementById("random-comment")
-              .appendChild(totalCommentsElement);
-            document
-              .getElementById("random-comment")
-              .appendChild(winnerCommentsElement);
-
-            fetch(
-              `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&maxResults=10&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk&authorChannelId=${channelId}`
-            )
-              .then((response) => response.json())
-              .then((data) => {
-                const commentThreads = data.items;
-                const winnerCommentsCount = commentThreads.filter(
-                  (commentThread) =>
-                    commentThread.snippet.topLevelComment.snippet
-                      .authorChannelId.value === channelId
-                ).length;
-
-                winnerCommentsSpan.textContent = winnerCommentsCount;
-              })
-              .catch((error) => {
-                console.error("Произошла ошибка:", error);
-                alert(
-                  "Произошла ошибка при получении количества комментариев от победителя"
-                );
-              });
-          })
-          .catch((error) => {
-            console.error("Произошла ошибка:", error);
-            alert(
-              "Произошла ошибка при получении информации о канале комментатора"
-            );
-          });
-
-          fetch(
-            `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&maxResults=10&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk`
-          )
-            .then((response) => response.json())
-            .then((data) => {
-              const allUsers = {};
-              data.items.forEach((commentThread) => {
-                const userName =
-                commentThread.snippet.topLevelComment.snippet.authorDisplayName;
-                allUsers[userName] = (allUsers[userName] || 0) + 1;
-              });
-        
-              filterParticipatingUsers = Object.keys(allUsers).filter(
-                (userName) => allUsers[userName] >= 2
-              );
-              const com2bList = document.querySelector('.com2b-list');
-              com2bList.innerHTML = '';
-
-              filterParticipatingUsers.forEach((user) => {
-                const newUserElement = document.createElement('div');
-                newUserElement.classList.add('com2b-item');
-                newUserElement.textContent = user;
-                com2bList.appendChild(newUserElement);
-                const newUserElementHover = document.createElement('div');
-                newUserElementHover.classList.add('com2b-item-hover');
-                newUserElementHover.textContent = 'СКОПИРОВАТЬ';
-                newUserElement.appendChild(newUserElementHover);
-                
-                newUserElement.onclick = function(){
-                  navigator.clipboard.writeText(user);
-                }
-              });
-
-              console.log(participatingUsers);
-            })
-            .catch((error) => {
-              console.error("Произошла ошибка:", error);
-              alert("Произошла ошибка при получении списка комментариев");
-            });
+          winnerCommentsSpan.textContent = winnerCommentsCount;
+        })
+        .catch((error) => {
+          console.error("Произошла ошибка:", error);
+          alert(
+            "Произошла ошибка при получении количества комментариев от победителя"
+          );
+        });
       })
       .catch((error) => {
         console.error("Произошла ошибка:", error);
-        alert("Произошла ошибка при получении комментариев");
+        alert(
+          "Произошла ошибка при получении информации о канале комментатора"
+        );
       });
+
+      fetch(
+        `https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId=${videoId}&maxResults=100&key=AIzaSyDqHnVhdAh0lMy0SR5rIMo785bWIMmohvk`
+      )
+      .then((response) => response.json())
+      .then((data) => {
+        const allUsers = {};
+        data.items.forEach((commentThread) => {
+          const userName =
+          commentThread.snippet.topLevelComment.snippet.authorDisplayName;
+          allUsers[userName] = (allUsers[userName] || 0) + 1;
+        });
+  
+        filterParticipatingUsers = Object.keys(allUsers).filter(
+          (userName) => allUsers[userName] >= 2
+        );
+        const com2bList = document.querySelector('.com2b-list');
+        com2bList.innerHTML = '';
+
+        filterParticipatingUsers.forEach((user) => {
+          const newUserElement = document.createElement('div');
+          newUserElement.classList.add('com2b-item');
+          newUserElement.textContent = user;
+          com2bList.appendChild(newUserElement);
+          const newUserElementHover = document.createElement('div');
+          newUserElementHover.classList.add('com2b-item-hover');
+          newUserElementHover.textContent = 'СКОПИРОВАТЬ';
+          newUserElement.appendChild(newUserElementHover);
+          
+          newUserElement.onclick = function(){
+            navigator.clipboard.writeText(user);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error("Произошла ошибка:", error);
+        alert("Произошла ошибка при получении списка комментариев");
+      });
+    })
+    .catch((error) => {
+      console.error("Произошла ошибка:", error);
+      alert("Произошла ошибка при получении комментариев");
+    });
 
     document.querySelector(".random-comment").classList.remove("none");
     document.getElementById("random-comment").classList.add("spin1");
@@ -241,8 +234,6 @@ function getRandomComment() {
       document.getElementById("random-comment").classList.remove("spin1");
       document.getElementById("win").classList.remove("spin2");
     }, 2000);
-    
-    
   }, 100);
 }
 
